67a5179928dc1e36d085a1a599a634392d23b2e7
Dishant Joshi <141486325+dishantjoshi0808@users.noreply.github.com>
2024-01-02 14:29:02 +0530

    Form Pr
    Form Pr

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CommentReview from './Components/Comment';
+import FormClass from './Components/FormClass'; 
+
+function App() {
+  return (
+    <div className="App">
+      <FormClass/>
+
+      <CommentReview/>
+    </div>
+  );
+}
+
+export default App;
